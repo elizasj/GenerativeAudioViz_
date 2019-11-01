e34697ffc6d27da968cba62f540c2c3ef3fa8236
elizasj <eliza.struthersjobin@gmail.com>
2019-11-01 17:41:13 +0100

    first commit

---
+++ Assets/Scripts/NoteIndicator.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using MidiJack;
+using System.Runtime.InteropServices;
+
+
+public class NoteIndicator : MonoBehaviour
+{
+    public int noteNumber;
+    
+    float initialScale = 1;
+    float scale = 0;
+    float scaleAnim = 0;
+    float scalePerlin = 0;
+
+    public int channel = 0;
+    public int velocity = 0;
+
+
+    private MaterialPropertyBlock propBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        propBlock = new MaterialPropertyBlock();
+        initialScale = transform.localScale.x;
+    }
+
+    int _countToUpdate;
+    int _lastMessageCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ORCA
+
+        /* 
+        MidiMaster.GetKey(channel, noteNumber) 
+         
+        Returns the velocity value while the key is pressed, or zero
+        while the key is released. The value ranges from 0.0 (note-off) 
+        to 1.0 (maximum velocity).
+
+        The channel arguments can be omitted. In that case, the 
+        functions return the values in the All-Channel slot, 
+        which stores mixed status of all active channels.
+
+        Use channels to specify certain actions during live coding 
+        For ex,  you know anything you do to channel 0 affects scale,
+        anything you do to channel one affects rotation, etc.
+
+
+        The current MIDI specification calls for 16 MIDI channels. 
+        Channels accessed via MidiChannel
+
+        Ch1,    // 0
+        Ch2,    // 1
+        Ch3,    // ... etc
+        Ch4,
+        Ch5,
+        Ch6,
+        Ch7,
+        Ch8,
+        Ch9,
+        Ch10,
+        Ch11,
+        Ch12,
+        Ch13,
+        Ch14,
+        Ch15, 
+        Ch16,
+        All   
+
+        */
+
+
+        scaleAnim += MidiMaster.GetKey(MidiChannel.Ch2, noteNumber) / 2f; // note devided by 10
+        if (MidiMaster.GetKey(noteNumber) != 0) Debug.Log(MidiMaster.GetKey(noteNumber));
+        scaleAnim -= (scaleAnim - 0) / 5; // fade back to zero
+
+
+        // PERLIN
+        scalePerlin = Mathf.PerlinNoise((float)velocity / 10 + Time.time, (float)channel / 10);
+        scale = initialScale + scalePerlin + scaleAnim; // original scale, additional waviness, scale from notes from orca
+
+        /*scale += .01f;
+        scale = Mathf.Min(scale, initialScale);
+        transform.localScale = new Vector3(scale, scale, scale);*/
+
+
+        //scale = initialScale + scaleAnim;
+
+        transform.localScale = new Vector3(scale, scale, scale);
+
+        //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(noteNumber));
+        //transform.localScale = Vector3.one * (scale + MidiMaster.GetKey(noteNumber));
+        transform.Rotate(Vector3.one * (MidiMaster.GetKey(MidiChannel.Ch1, noteNumber)));
+
+        float value = (float)(noteNumber-23) / (128f-23f);
+
+        var randomColors = Random.ColorHSV(value, value, 1, 1, .5f, .5f);
+        var lerpColors = Color.Lerp(randomColors, Color.red, Mathf.PingPong(Time.time, value));
+        
+
+        var perlinColor = Color.Lerp(Color.white,Color.black, scalePerlin); // color from perlin animation
+
+        var color = Color.Lerp(perlinColor, Color.red, scaleAnim); // add red to the perlin animation
+
+        // color = Random.ColorHSV(value, value,1,1,.5f,.5f);
+        propBlock.SetColor("_Color", color);
+        GetComponent<Renderer>().SetPropertyBlock(propBlock);
+
+       // Debug.Log(noteNumber);
+    }
+}
---
+++ Assets/Scripts/PhyloPatternGen.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Phyllotaxis
+// https://www.youtube.com/watch?v=KWoJgHFYWxY
+
+public class PhyloPatternGen : MonoBehaviour
+{
+
+    int numOfShapes = 0; // object number
+    public float scaleOfRadius = 2f; // scaling of radius
+    public float pointiness = .3f; // pointiness in z space
+
+    // types of shapes possible
+    public GameObject sphere;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        for (int c = 0; c < 16; c++) //channel
+        {
+            for (int v = 0; v < 8; v++) //note = velocity
+            {
+
+                var angle = (i * 137.5f) * Mathf.Rad2Deg; // 137.3 // 137.6
+                var radius = scaleOfRadius * Mathf.Sqrt(i);
+
+                var x = radius * Mathf.Cos(angle);
+                var y = radius * Mathf.Sin(angle);
+                //var z = radius * Mathf.Atan(angle);
+                var z = radius * pointiness;
+
+                GameObject shape = Instantiate(cube, new Vector3(x, y, z), Quaternion.identity); // default pos 0,0,0
+                shape.name = "shape";
+                shape.transform.parent = GameObject.Find("Generator").transform;
+                shape.transform.LookAt(Vector3.zero, Vector3.forward);
+                //shape.transform.localScale = new Vector3(0, 0, 0);
+                shape.SetActive(true);
+
+                // connect shape to Orca
+                NoteIndicator noteIndicator = shape.AddComponent<NoteIndicator>();
+                noteIndicator.noteNumber = i;
+
+                noteIndicator.channel = c;
+                noteIndicator.velocity = v;
+
+                i++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (numOfShapes <= 105)
+        {
+
+            var angle = (numOfShapes * 137.5f) * Mathf.Rad2Deg; // 137.3 // 137.6
+            var radius = scaleOfRadius * Mathf.Sqrt(numOfShapes);
+
+            var x = radius * Mathf.Cos(angle);
+            var y = radius * Mathf.Sin(angle);
+            //var z = radius * Mathf.Atan(angle);
+            var z = radius * pointiness;
+
+            GameObject shape = Instantiate(cube, new Vector3(x, y, z), Quaternion.identity); // default pos 0,0,0
+            shape.name = "shape";
+            shape.transform.parent = GameObject.Find("Generator").transform;
+            shape.transform.LookAt(shape.transform.parent,  Vector3.left);
+            shape.SetActive(true);
+
+            // connect shape to Orca
+            NoteIndicator noteIndicator = shape.AddComponent<NoteIndicator>();
+            noteIndicator.noteNumber = numOfShapes;     
+        }
+
+        numOfShapes++;
+        */
+    }
+}
